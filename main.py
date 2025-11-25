2deb5d58de3fa590ebd8900bb62256f4139466ab
Renata Rocha <renata.rocha@idealo.de>
2025-11-25 17:33:06 +0100

    PREN-383: Implementing remove_expense and exit functions, adding id to the table and printing empty table message

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def add_expense():
     category = str(input(f"What category did you spend it in? Please select: {", ".join(categories)} \n")).title()
 
     expenses.append([expense, amount, date, category])
+    print(f"\nExpense added successfully!\n")
 
     show_expenses()
 
@@ -23,30 +24,50 @@ def show_expenses():
     """Prints the expense list in a table format in the terminal,
     asks the user for an action and calls the corresponding function"""
 
+    if not expenses:
+        user_choice = str(input("You don't have any expenses saved yet. Would you like to add one? Type 'Y' to add or 'N' to exit.\n")).lower()
+        if user_choice == "y":
+            add_expense()
+        else:
+            exit_program()
+
     print(
         tabulate(
-            expenses,
+            # creates a dynamic id starting with 1 for each expense to be displayed in the table
+            [[i + 1] + expense for i, expense in enumerate(expenses)],
             headers=['Expense', 'Amount (€)', 'Date', 'Category'],
             tablefmt="fancy_grid",
-            showindex="always",
             floatfmt=".2f"
         )
     )
 
-    user_choice = str(input("\n What would you like to do? To add an expense, type 'add', to remove an expense, type 'remove', "
+    next_action = str(input("\nWhat would you like to do? To add an expense, type 'add', to remove an expense, type 'remove', "
           "to calculate the total amount spent, type 'total' or to exit, type 'exit'.\n")).lower()
-    functions[user_choice]()
+    functions[next_action]()
 
-# def remove_expense():
-#
+def remove_expense():
+    """Removes the expense at the specified index from the expense list"""
+
+    user_choice = str(input("Which expense would you like to remove? Type the number of the row or 'cancel' to cancel.\n"))
+    if user_choice == "cancel":
+        show_expenses()
+    else:
+        index = int(user_choice) - 1
+        expenses.pop(index)
+        print(f"Expense removed successfully!")
+        show_expenses()
+
 # def calculate_total():
 #
 # def save_file():
 #
-# def exit():
+def exit_program():
+    print("Thanks for using Expense Tracker!")
 
 functions = {
     "add": add_expense,
+    "remove": remove_expense,
+    "exit": exit_program,
 }
 
 if expenses:
