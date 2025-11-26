55bc33dc24e45df3a53fbea8b1b2c82f71e5c684
Renata Rocha <renata.rocha@idealo.de>
2025-11-26 15:37:01 +0100

    PREN-383: Implementing input validation

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ categories = ['Food', 'Transport', 'Entertainment', 'Healthcare', 'Other']
 print(logo)
 print("Welcome to your expense tracker!\n")
 
+def get_valid_input(prompt, data_type):
+    """Continuously asks for input until the input is valid and returns it as the specified data type"""
+
+    while True:
+        try:
+            user_input = input(prompt)
+            return data_type(user_input)
+        except ValueError:
+            print(f"\nInvalid input. Please try again.\n")
+
 def add_expense():
     """Asks the user for expense name, amount, date and category and adds it to the expense list"""
 
-    expense = str(input("\nWhat did you spend? \n"))
-    amount = float(input("How much did you spend?\n€ "))
-    date = str(input("When did you spend it? Please use the format DD/MM/YYYY\n"))
-    category = str(input(f"What category did you spend it in? Please select: {", ".join(categories)} \n")).title()
+    expense = get_valid_input("\nWhat did you spend? \n", str)
+    amount = get_valid_input("How much did you spend?\n€ ", float)
+    date = get_valid_input("When did you spend it? Please use the format DD/MM/YYYY\n", str)
+    category = get_valid_input(f"What category did you spend it in? Please select or type a new one: {", ".join(categories)} \n", str)
 
     expenses.append([expense, amount, date, category])
     print(f"\nExpense added successfully!")
@@ -25,6 +35,16 @@ def add_expense():
 def get_next_action():
     """Asks the user for the next action and calls the corresponding function"""
 
+    if not expenses:
+        print("\nYou don't have any expenses yet.")
+        user_choice = get_valid_input("Type 'add' to add an expense, 'save' to save or 'exit' to exit.\n", str)
+
+        if user_choice in ["add", "save", "exit"]:
+            functions[user_choice]()
+        else:
+            print(f"\nInvalid action. Please try again.\n")
+            get_next_action()
+
     next_action = str(
         input("\nWhat would you like to do?"
               "\nTo add an expense, type 'add'"
@@ -33,6 +53,11 @@ def get_next_action():
               "\nto calculate the total amount spent, type 'total'"
               "\nto save your expenses, type 'save'"
               "\nor to exit, type 'exit'.\n")).lower()
+
+    if next_action not in functions:
+        print(f"\nInvalid action. Please try again.\n")
+        get_next_action()
+
     functions[next_action]()
 
 def show_expenses():
@@ -40,7 +65,6 @@ def show_expenses():
     asks the user for an action and calls the corresponding function"""
 
     if not expenses:
-        print("\nYou don't have any expenses saved yet.")
         get_next_action()
 
     print(f"\n{
@@ -58,14 +82,19 @@ def show_expenses():
 def remove_expense():
     """Removes the expense at the specified index from the expense list"""
 
-    user_choice = str(input("\nWhich expense would you like to remove? Type the number of the row or 'cancel' to cancel.\n"))
-    if user_choice == "cancel":
+    user_choice = get_valid_input("\nWhich expense would you like to remove? Type the number of the row or 0 to cancel.\n", int)
+
+    if user_choice == 0:
         show_expenses()
-    else:
-        index = int(user_choice) - 1
+    elif user_choice - 1 in range(len(expenses)):
+        index = user_choice - 1
         expenses.pop(index)
+
         print(f"\nExpense removed successfully!")
         show_expenses()
+    else:
+        print(f"\nInvalid choice. Please try again.\n")
+        remove_expense()
 
 def calculate_total():
     """Calculates the total amount spent and prints it in the terminal"""
@@ -111,6 +140,6 @@ try:
         print("\nThese are your current expenses:")
         show_expenses()
     else:
-        add_expense()
+        get_next_action()
 except FileNotFoundError:
-    add_expense()
+    get_next_action()
